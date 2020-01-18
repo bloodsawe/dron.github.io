b281e3842ae440a1ff4733cebe1b6ce985b893c4
rbkv-dev <rbkv.dev@gmail.com>
2020-01-18 15:54:58 +0200

    careers-page add

--- src/js/about.js
+++ src/js/about.js
@@ -1,18 +1,20 @@
-
 document.addEventListener("DOMContentLoaded", function () {
-  if (document.querySelectorAll(".aboutUs").length > 0) {
-    var blockLeft = document.querySelector(".our-awards .video-description__text").offsetLeft;
-    var h1Left = document.querySelector(".our-awards .video-description__text h1").offsetLeft; // console.log(blockLeft, h1Left);
+//   if (document.querySelectorAll(".aboutUs").length > 0) {
+//     var blockLeft = document.querySelector(".our-awards .video-description__text").offsetLeft;
+//     var h1Left = document.querySelector(".our-awards .video-description__text h1").offsetLeft; // console.log(blockLeft, h1Left);
 
-    document.querySelector(".our-awards .video-description__text").style.left = "".concat(blockLeft + h1Left + 10, "px");
-  }
+//     document.querySelector(".our-awards .video-description__text").style.left = "".concat(blockLeft + h1Left + 10, "px");
+//   }
 
   if (document.querySelectorAll(".aboutUs").length > 0) {
-    var dropDown = Array.from(document.querySelectorAll(".awards__arrow"));
+    let dropDown = Array.from(document.querySelectorAll(".awards__arrow"));
     dropDown.forEach(function (elem, index) {
       elem.addEventListener("click", function (e) {
-        var list = document.querySelectorAll(".awards__list")[index];
+        let list = document.querySelectorAll(".awards__list")[index];
         list.classList.toggle("active");
+        let arrow = document.querySelectorAll(".awards__arrow")[index]; 
+              
+        (arrow.className == "active") ? arrow.style.transform = "rotate(-90deg)" : arrow.style.transform = "rotate(90deg)";
       });
     });
   }
