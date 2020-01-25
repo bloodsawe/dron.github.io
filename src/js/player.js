90196d9ccefc08180df4f1d55f744a94afac9445
1revolman1 <1revolman1@gmail.com>
2020-01-25 22:36:22 +0200

    add

---
+++ src/js/player.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded",function() {
+  if(document.querySelector(".sectors-oil,.services,.services-collection")) {
+    // console.log('123123');
+    
+    function init() {
+      _video = document.getElementById("video");
+      _playBtn = document.getElementById("playBtn");
+      _pauseBtn = document.getElementById("pauseBtn");
+      // console.log(_video);
+      
+    }
+    document.addEventListener("DOMContentLoaded", init(), false);
+  }
+});
--- src/js/team.js
+++ src/js/team.js
@@ -1,20 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
-  if(document.querySelector(".team-page")) {
+  if (document.querySelector(".team-page")) {
     document.querySelector(".button").classList.add("active-button");
     document.querySelector(".block-with-employee").style.display = "flex";
   }
+  if (document.querySelector(".newsandblog")) {
+    document
+      .querySelector(".filter-block__item")
+      .classList.add("active-button");
+    document.querySelector(".blog-cards").style.display = "block";
+  }
 });
-
-function openSelection(evt) {
+function openSelection(
+  evt,
+  blockThatNeedToOpen = "block-with-employee",
+  button = "active-button",
+  displayStyle = "flex"
+) {
   var i;
-  type=event.target.getAttribute("data-categories");
-  document.querySelector(".active-button").classList.remove("active-button");
-  if (evt.target.classList.contains("active-button"))
-    evt.target.classList.remove("active-button");
-  else evt.target.classList.add("active-button");
-  var x = document.getElementsByClassName("block-with-employee");
+  type = event.target.getAttribute("data-categories");
+  document.querySelector(`.${button}`).classList.remove(`${button}`);
+  if (evt.target.classList.contains(`${button}`))
+    evt.target.classList.remove(`${button}`);
+  else evt.target.classList.add(`${button}`);
+  var x = document.getElementsByClassName(`${blockThatNeedToOpen}`);
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  document.getElementById(type).style.display = "flex";
+  document.getElementById(type).style.display = displayStyle;
+  ScrollReveal().sync();
 }
