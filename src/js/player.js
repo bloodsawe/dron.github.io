b440a75635cf8550f7090465159310753416483c
Aleksandr Dolinovskiy <adolinovskiy@gmail.com>
2020-01-27 17:11:44 +0200

    player

--- src/js/player.js
+++ src/js/player.js
@@ -1,13 +1,17 @@
 document.addEventListener("DOMContentLoaded",function() {
   if(document.querySelector(".sectors-oil,.services,.services-collection")) {
-    // console.log('123123');
-    
     function init() {
       _video = document.getElementById("video");
+      _video.addEventListener("pause", () => {
+        _playBtn.style.display = 'block';
+        
+      })
+      _video.addEventListener("play", () => {
+        _playBtn.style.display = 'none';
+        
+      })
       _playBtn = document.getElementById("playBtn");
       _pauseBtn = document.getElementById("pauseBtn");
-      // console.log(_video);
-      
     }
     document.addEventListener("DOMContentLoaded", init(), false);
   }
