ba796006bbffbb10b12e02a58cd52ae9493a1002
bloodsawe <adolinovskiy@gmail.com>
2020-03-26 09:43:52 +0200

    26-03-2020

--- src/js/player.js
+++ src/js/player.js
@@ -2,11 +2,12 @@ document.addEventListener("DOMContentLoaded",function() {
   if(document.querySelector(".aboutUs, .services, .careers-page, .sectors-oil, .sectors-grid, .sectors-generation, .sectors-construction, .services-collection")) {
     function init() {
       _video = document.getElementById("video");
+      _video.play(); 
+      _video.pause();
 
-      // Show loading animation.
+
 
       // var playPromise = _video.play();
-    
       // if (playPromise !== undefined) {
       //   playPromise.then(_ => {
       //     _video.pause();
@@ -14,18 +15,40 @@ document.addEventListener("DOMContentLoaded",function() {
       //   .catch(error => {});
       // }
 
-      setTimeout(() => {
-        _video.play(); 
-        _video.pause();
-      }, 1000)
+
 
       _video.addEventListener("pause", () => _playBtn.style.display = 'block')
       _video.addEventListener("play", () =>_playBtn.style.display = 'none')
       _video.addEventListener("seeking", () =>_playBtn.style.display = 'none')
       _playBtn = document.getElementById("playBtn");
-      _pauseBtn = document.getElementById("pauseBtn");
+      // _pauseBtn = document.getElementById("pauseBtn");
       _video.addEventListener("loadeddata", () => { ScrollReveal().sync() })
+
+      if (navigator.userAgent.search(/Safari/) && navigator.userAgent.search(/Mobile/) && window.outerWidth <= 375) {
+        // alert('Safari Mobile');
+        _playBtn.style.display = 'none';
+      }
+
     }
     document.addEventListener("DOMContentLoaded", init(), false);
   }
 });
+
+
+
+
+
+// _video.addEventListener("loadeddata", () => { _video.play(); _video.pause(); })
+
+// var playPromise = _video.play();
+// if (playPromise !== undefined) {
+//   playPromise.then(_ => {
+//     _video.pause();
+//   })
+//   .catch(error => {});
+// }
+
+// setTimeout(() => {
+//   _video.play(); 
+//   _video.pause();
+// }, 1000)
